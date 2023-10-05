79ba7e32f1b3286265fd698498510b863b4a6d65
phoenix-ZY <hth_2003@sjtu.edu.cn>
2023-10-05 16:17:56 +0800

    update

--- index/models.py
+++ index/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from users.models import User
+import requests
+from bs4 import BeautifulSoup
 
 
 # Create your models here.
@@ -7,9 +9,40 @@ class LINK(models.Model):
     url = models.URLField(max_length=100, help_text="Enter a link of a website")
     name = models.CharField(max_length=100, help_text="Enter the name of a website")
     user = models.ManyToManyField(User)
+    image = models.ImageField(upload_to="link_image", null=True, blank=True)
 
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
         """
         return self.url
+
+    @property
+    def initial(self):
+        return self.name[0].upper()
+
+    @property
+    def image_url(self):
+        if self.image == "":
+            # return "/media/static_default/{}.png".format(self.initial)
+            headers = {
+                "User-Agent": (
+                    "Mozilla/5.0 (Windows NT 6.2; WOW64) "
+                    "AppleWebKit/537.36 (KHTML, like Gecko) "
+                    "Chrome/30.0.1599.17 Safari/537.36"
+                )
+            }  # 伪装请求头
+            response = requests.get(self.url, headers=headers)
+            soup = BeautifulSoup(response.text, "html.parser")
+            favicon = soup.find("link", href=True, type="image/x-icon")["href"]
+            if favicon:
+                favicon_resp = requests.get(favicon, headers=headers)
+                domain = self.url.split("//")[-1].split(".")[0]
+                path = "/media/link_image/"
+                fullpath = domain + path
+                with open(fullpath, "wb") as f:
+                    f.write(favicon_resp.content)
+                self.image.url = fullpath
+            else:
+                self.image.url = "/media/static_default/{}.png".format(self.initial)
+        return self.image.url
--- index/views.py
+++ index/views.py
@@ -14,17 +14,15 @@ from django.db import IntegrityError
 def index(request: HttpRequest):
     username = request.user.username
     user = get_object_or_404(User, username=username)
-    if request.method == "GET":
-        link_form = linkform()
-
-    elif request.method == "POST":
+    if request.method == "POST":
         link_form = linkform(request.POST)
         if link_form.is_valid():
             link_url = link_form.cleaned_data["add_link"]
             link_name = link_form.cleaned_data["add_name"]
-
-            my_link = LINK.objects.filter(name = link_url)
-            if(my_link):
+            if not link_url.startswith("https://"):
+                link_url = "https://" + link_url
+            my_link = LINK.objects.filter(name=link_url)
+            if my_link:
                 my_link[0].user.add(request.user)
             else:
                 my_link = LINK.objects.create(url=link_url, name=link_name)
